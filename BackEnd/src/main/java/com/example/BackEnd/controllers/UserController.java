7c0d4145e507cfa722f7e040f6d7bc570c74398c
Philasande Bhani <pbhanina@gmail.com>
2023-08-18 11:21:07 +0200

    Saving my certificates

--- BackEnd/src/main/java/com/example/BackEnd/controllers/UserController.java
+++ BackEnd/src/main/java/com/example/BackEnd/controllers/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/ ")
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     UserService service;
